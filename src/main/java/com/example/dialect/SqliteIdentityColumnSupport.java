8488726a7a67a7cecf758d8bedcac0a636ba39c8
Igor Payenskii <38349979+Lvivsky@users.noreply.github.com>
2020-06-18 22:33:38 +0300

    add all project

---
+++ src/main/java/com/example/dialect/SqliteIdentityColumnSupport.java
@@ -0,0 +1,22 @@
+package com.example.dialect;
+
+import org.hibernate.MappingException;
+import org.hibernate.dialect.identity.IdentityColumnSupportImpl;
+
+public class SqliteIdentityColumnSupport extends IdentityColumnSupportImpl {
+    @Override
+    public boolean supportsIdentityColumns() {
+        return true;
+    }
+
+    @Override
+    public String getIdentitySelectString(String table, String column, int type)
+            throws MappingException {
+        return "select last_insert_rowid()";
+    }
+
+    @Override
+    public String getIdentityColumnString(int type) throws MappingException {
+        return "integer";
+    }
+}
