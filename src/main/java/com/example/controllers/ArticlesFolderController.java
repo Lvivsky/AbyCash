5c8d6cdd30194786cb7bc9b45a39b44abd171b79
Igor Payenskii <38349979+Lvivsky@users.noreply.github.com>
2020-06-21 17:15:23 +0300

    add edit category

--- src/main/java/com/example/controllers/ArticlesFolderController.java
+++ src/main/java/com/example/controllers/ArticlesFolderController.java
@@ -1,14 +1,20 @@
 package com.example.controllers;
 
 import com.example.models.Categories;
+import com.example.models.Currencies;
 import com.example.repositories.CategoriesRepo;
 import com.example.repositories.UsersRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ArticlesFolderController {
@@ -32,22 +38,60 @@ public class ArticlesFolderController {
         return "articles_folder";
     }
 
-    @GetMapping("/articles_folder/add")
-    public String addCategory() {
+    @GetMapping("/articles_folder/{id}/add")
+    public String addCategory(@PathVariable(value = "id") int id, Model model) {
         return "/fragments/addCategory";
     }
 
-    @PostMapping("/articles_folder/add")
+    @PostMapping("/articles_folder/{id}/add")
     public String addOutcome (
+            @PathVariable(value = "id") int id,
             @RequestParam String name,
             @RequestParam String comment,
             Model model) {
 
         System.out.println(name);
         System.out.println(comment);
-        Categories categories = new Categories(name, comment, 2);
+        Categories categories = new Categories(name, comment, id);
         categoriesRepo.save(categories);
         return "redirect:/articles_folder";
     }
 
+    /*
+    @GetMapping("/articles_folder/{id}/edit")
+    public String editCategoryGet(@PathVariable(value = "id") int id, Model model) {
+        Optional<Categories> categories = categoriesRepo.findById(id);
+        ArrayList<Categories> res = new ArrayList<Categories>();
+        categories.ifPresent(res::add);
+
+        model.addAttribute("curr-categ", res);
+        return "fragments/editCategory";
+    }*/
+
+    @GetMapping("/articles_folder/{id}/edit")
+    public String editCategoryGet(@PathVariable(value = "id") int id, Model model) {
+        if (!categoriesRepo.existsById(id)) {
+            return "redirect:/articles_folder";
+        }
+
+        Optional<Categories> categories = categoriesRepo.findById(id);
+        ArrayList<Categories> res = new ArrayList<Categories>();
+        categories.ifPresent(res::add);
+        model.addAttribute("curr", res);
+
+        return "fragments/editCategory";
+    }
+
+    @PostMapping("/articles_folder/{id}/edit")
+    public String editCategoryPost(@PathVariable(value = "id") int id,
+                               @RequestParam String name,
+                               @RequestParam String comment, Model model) {
+
+        Categories category = categoriesRepo.findById(id).orElseThrow();
+        category.setName(name);
+        category.setComment(comment);
+        categoriesRepo.save(category);
+
+        return "redirect:/articles_folder";
+    }
 }
--- src/main/java/com/example/repositories/CategoriesRepo.java
+++ src/main/java/com/example/repositories/CategoriesRepo.java
@@ -15,18 +15,10 @@ public interface CategoriesRepo extends CrudRepository<Categories, Integer> {
     @Query(value = "SELECT * FROM Categories WHERE Parent = 2", nativeQuery = true)
     List<Categories> findOutcome();
 
-    @Query(value = "INSERT INTO Categories VALUES (:id,:guid,:changed,:deleted,:name,:comment,:parent)",
-            nativeQuery = true)
-    void insertValueOutcome(@Param("id") int id,
-                            @Param("guid") String guid,
-                            @Param("changed") String changed,
-                            @Param("deleted") String deleted,
-                            @Param("name") String name,
-                            @Param("comment") String comment,
-                            @Param("parent") String parent
+    @Query(value = "SELECT * FROM Categories WHERE Id = :id", nativeQuery = true)
+    Categories findCategoryById(@Param("id") int id);
 
-    );
 
-    @Query(value = "INSERT INTO Categories VALUES (100,'fdsf',34,'0','їсти','шось','2')", nativeQuery = true)
-    void i();
+
+
 }
