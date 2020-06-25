cf3476ee7d9526a7db01f686d2edc843b94819ce
Igor Payenskii <38349979+Lvivsky@users.noreply.github.com>
2020-06-25 11:26:20 +0300

    fixed some bugs

--- src/main/java/com/example/repositories/TransactionsRepo.java
+++ src/main/java/com/example/repositories/TransactionsRepo.java
@@ -1,13 +1,18 @@
 package com.example.repositories;
 
+import com.example.models.dbmodels.Accounts;
 import com.example.models.dbmodels.Transactions;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 
 public interface TransactionsRepo extends CrudRepository<Transactions, Integer> {
 
     @Query(value = "SELECT MAX(Id) FROM Transactions", nativeQuery = true)
     int findLatestId();
+
+    @Query(value = "SELECT * FROM Transactions where IncomeAccount = :id OR ExpenseAccount = :id", nativeQuery = true)
+    Transactions findIncomeAccount(@Param(value = "id") int id);
 
 }
